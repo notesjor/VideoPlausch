70a984934259bc3358340530f90e9bbfd3e0da6b
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2020-07-17 05:00:12 +0200

    // commit

---
+++ PlauschiServer/PlauschiServer/Model/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlauschiServer.Model
+{
+  public class Event
+  {
+    public Event(int max)
+    {
+      Guid = Guid.NewGuid();
+      Max = max;
+      Count = 0;
+    }
+
+    public Guid Guid { get; set; }
+    public int Max { get; set; }
+    public int Count { get; set; }
+  }
+}
---
+++ PlauschiServer/PlauschiServer/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using PlauschiServer.Model;
+using Tfres;
+
+namespace PlauschiServer
+{
+  class Program
+  {
+    private static object _lock = new object();
+    private static Dictionary<string, Event> _events = new Dictionary<string, Event>();
+
+    private const int _maxUsersPerGroup = 15;
+    private const int _maxEvents = 1000;
+
+    static void Main(string[] args)
+    {
+      var server = new Server("*", 8123, ctx => ctx.Response.Send(HttpStatusCode.InternalServerError));
+      server.AddEndpoint(HttpVerb.GET, "/new", NewGroupMember);
+      server.AddEndpoint(HttpVerb.GET, "/check", CheckGroup);
+    }
+
+    private static Task CheckGroup(HttpContext ctx)
+    {
+      try
+      {
+        var get = ctx.Request.GetData();
+        var eventId = get["event"];
+        var groupId = Guid.Parse(get["group"]);
+
+        lock (_lock)
+        {
+          if (!_events.ContainsKey(eventId))
+            return ctx.Response.Send(HttpStatusCode.InternalServerError);
+
+          var myEvent = _events[eventId];
+          return myEvent.Guid != groupId ? ctx.Response.Send(HttpStatusCode.Created) : ctx.Response.Send(myEvent);
+        }
+      }
+      catch
+      {
+        return ctx.Response.Send(HttpStatusCode.InternalServerError);
+      }
+    }
+
+    private static Task NewGroupMember(HttpContext ctx)
+    {
+      try
+      {
+        var get = ctx.Request.GetData();
+        var eventId = get["event"];
+        var groupSize = int.Parse(get["size"]);
+        if (groupSize > _maxUsersPerGroup)
+          return ctx.Response.Send(HttpStatusCode.InternalServerError);
+
+        lock (_lock)
+        {
+          if (!_events.ContainsKey(eventId))
+          {
+            if (_events.Count > _maxEvents)
+              return ctx.Response.Send(HttpStatusCode.InternalServerError);
+            _events.Add(eventId, new Event(groupSize));
+          }
+
+          var myEvent = _events[eventId];
+          myEvent.Count++;
+
+          _events[eventId] = myEvent.Count >= myEvent.Max ? new Event(groupSize) : myEvent;
+
+          return ctx.Response.Send(myEvent);
+        }
+      }
+      catch
+      {
+        return ctx.Response.Send(HttpStatusCode.InternalServerError);
+      }
+    }
+  }
+}
